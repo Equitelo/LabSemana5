d0f9861828e8295ec61be62b5f23dd3aeca8d93d
Equitelo <samuel_lara@unitec.edu>
2024-08-16 09:34:46 -0600

    Cuando se ingresa la opcion 2 se pide ingresar numeros enteros, luego se le pregunta al usuario con un do while con una respuesta String la cual la inicializamos con "no" y luego de ingresar el entero ahi le preguntamos si desea ingresar, todo esto dentro del do while. Lo hacemos con ciclo ya que veremos cual fue el numero mayor contro while e incilizando el mayor en 0 y si el numero ingresado es mayor a esa variable agarra el valor de dicha variable. Ponemos un contador dentro del ciclo para poder realizar el promedio junto a un acumulador.

--- src/laboratorioweek5/LaraSamuelE1.java
+++ src/laboratorioweek5/LaraSamuelE1.java
@@ -28,6 +28,27 @@ public class LaraSamuelE1 {
             if(menu==1){
             
             }else if(menu==2){
+                int mayor=0;
+                //para promedio
+                int vecesIngreso=0;
+                int suma=0;
+                double promedio=0;
+                String respuesta="no";
+                do{
+                    vecesIngreso++;
+                    System.out.print("Ingresar numeros enteros: ");
+                    int numeros=lea.nextInt();
+                    
+                    while(numeros>mayor){
+                        mayor=numeros;
+                    }
+                    suma+=numeros;
+                    promedio=suma/vecesIngreso;
+                    System.out.println("Desea terminar de ingresar numeros?(SI/NO): ");
+                    respuesta=lea.next().toLowerCase();
+                }while(respuesta.equals("no"));
+                System.out.println("El numero mayor ingresado: "+mayor);
+                System.out.println("El promedio de los numeros ingresados: "+promedio);
             }else if(menu==3){
             }else if(menu==4){
             }else{
